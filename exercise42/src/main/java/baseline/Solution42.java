f04c4b3db1095804acd7963c9fc35a1218d5da01
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-11 02:17:11 -0400

    Added pseudocode/plantUML diagram to exercise 42

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution42
+{
+    public static void main(String[] args)
+    {
+        // check to see if there is a file
+        // take in each line in the file to a list of strings
+        // use string.split to separate a line into multiple strings, and create spaces for formatting accordingly
+        // output each string in the correct format in a new file
+    }
+}
