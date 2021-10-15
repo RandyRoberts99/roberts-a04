f222c3ad1488bd4c16dc613760dca36b6c71ac2d
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-15 13:42:34 -0400

    Added exercise 45 pseudocode/plantUML document

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,23 @@
+package baseline;
+
+public class Solution45
+{
+    public static void main(String[] args)
+    {
+        // Create a scanner for reading in lines from a file
+        // For each line in the input file, take it as a string, replace all instances of "utilitze" with "use"
+        // take the new string and output it into an output file
+    }
+    private void scanFile()
+    {
+
+    }
+    public void replaceKeywordsInString()
+    {
+
+    }
+    private void printToFile()
+    {
+
+    }
+}
